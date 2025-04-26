ebde518c2183d8367151713eebf5e8b2d6122470
emtadeveloper <emta137671@gmail.com>
2025-04-26 10:34:26 +0330

    fix (debug) delete module article category  approval

--- config/setupRoutes.mjs
+++ config/setupRoutes.mjs
@@ -7,8 +7,6 @@ import userRouter from "../module/user/user.route.mjs";
 import themeRouter from "../module/theme/theme.route.mjs";
 import cityRouter from "../module/city/city.route.mjs";
 import roleRouter from "../module/role/role.route.mjs";
-import categoryRouter from "../module/category/category.route.mjs";
-import articleRouter from "../module/article/article.route.mjs";
 import constant from '../common/constant/index.mjs';
 import checkPermissions from "../common/middleware/checkPermissions.mjs"
 import Auth from "../common/middleware/authMiddleware.mjs";
@@ -25,8 +23,6 @@ const setupRoutes = async (app) => {
     app.use(`/api/${version}/theme`, Auth, themeRouter);
     app.use(`/api/${version}/locations`, cityRouter);
     app.use(`/api/${version}/role`, Auth, checkPermissions(constant.PERMISSIONS.ADMIN), roleRouter);
-    app.use(`/api/${version}/category`, Auth, checkPermissions(constant.PERMISSIONS.ADMIN), categoryRouter);
-    app.use(`/api/${version}/articles`, Auth, checkPermissions(constant.PERMISSIONS.ADMIN), articleRouter);
 };
 
 export default setupRoutes
